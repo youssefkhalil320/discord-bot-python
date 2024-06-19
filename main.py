b246685a3cf41529bda76dcf0a372539b0c659e0
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-19 07:54:21 +0300

    Ayah response

--- main.py
+++ main.py
@@ -8,3 +8,48 @@ from modules.responses import get_response
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Message was empty)")
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+@client.event
+async def on_ready() -> None:
+    print(f'Logged in as {client.user}')
+
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'{username} in {channel}: {user_message}')
+    await send_message(message, user_message)
+
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == '__main__':
+    main()
--- modules/responses.py
+++ modules/responses.py
@@ -1,2 +1,43 @@
-def get_response():
-    pass
+import requests
+import random
+
+
+def get_random_aya() -> str:
+    try:
+        verse_num = random.randint(1, 6346)
+        # Fetch a random Aya from the API
+        response = requests.get(
+            f"https://api.alquran.cloud/v1/ayah/{verse_num}/ar.asad")
+        response.encoding = 'utf-8'  # Ensure the response is interpreted as UTF-8
+        data = response.json()
+
+        if response.status_code == 200 and data['status'] == "OK":
+            # Extract Aya text and reference
+            aya_text = data['data']['text']
+            surah_name = data['data']['surah']['englishName']
+            surah_number = data['data']['surah']['number']
+            aya_number = data['data']['numberInSurah']
+
+            print(f"Aya from Surah {surah_name}, Aya {aya_number}: {aya_text}")
+            return f"Aya from Surah {surah_name}, Aya {aya_number}: {aya_text}"
+        else:
+            return "Could not fetch a random Aya at the moment."
+    except Exception as e:
+        return f"Error occurred: {str(e)}"
+
+
+def get_response(user_input: str) -> str:
+    lowered = user_input.lower()
+
+    if lowered == '':
+        return "Wanna talk?"
+    elif 'hello' in lowered:
+        return "Hello"
+    elif 'hi' in lowered:
+        return "Hi"
+    elif 'bye' in lowered:
+        return "Bye"
+    elif 'mnl' in lowered:
+        return get_manzil_data()
+    else:
+        return get_random_aya()
